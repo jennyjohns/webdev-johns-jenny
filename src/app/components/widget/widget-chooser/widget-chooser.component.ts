0ea695b8fff7112477f2f4edf3e82d2f0c09100c
jennyjohns <johns.j@husky.neu.edu>
2017-10-10 18:41:38 -0400

    Updated widget list

--- src/app/components/widget/widget-chooser/widget-chooser.component.ts
+++ src/app/components/widget/widget-chooser/widget-chooser.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WidgetService} from '../../../services/widget.service.client';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-widget-chooser',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widget-chooser.component.css']
 })
 export class WidgetChooserComponent implements OnInit {
-
-  constructor() { }
+  widgetId: string;
+  widgetType: string;
+  pageId: string;
+  userId: string;
+  webId: string;
+  size: number;
+  widgets = [];
+  constructor(private widgetService: WidgetService, private activatedRoute: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.pageId = params['pid'];
+          this.userId = params['uid'];
+          this.webId = params['wid'];
+        }
+      );
+    this.widgets = this.widgetService.findWidgetsByPageId(this.pageId);
   }
 
 }
--- src/app/components/widget/widget-list/widget-list.component.ts
+++ src/app/components/widget/widget-list/widget-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {WidgetService} from '../../../services/widget.service.client';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-widget-list',
@@ -11,18 +11,33 @@ export class WidgetListComponent implements OnInit {
   widgetId: string;
   widgetType: string;
   pageId: string;
+  userId: string;
+  webId: string;
   size: number;
- widgets = [];
-  constructor(private widgetService: WidgetService, private activatedRoute: ActivatedRoute) { }
+  widgets = [];
+  constructor(private widgetService: WidgetService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.activatedRoute.params
       .subscribe(
         (params: any) => {
           this.pageId = params['pid'];
+          this.userId = params['uid'];
+          this.webId = params['wid'];
         }
       );
     this.widgets = this.widgetService.findWidgetsByPageId(this.pageId);
   }
-
+  goToProfile() {
+    this.router.navigate(['user/', this.userId]);
+  }
+  goToPages() {
+    this.router.navigate(['user/', this.userId, 'website', this.webId, 'page']);
+  }
+  newWidget() {
+    this.router.navigate(['user/', this.userId, 'website', this.webId, 'page', this.pageId, 'widget', 'new']);
+  }
+  editWidget(wgid) {
+    this.router.navigate(['user/', this.userId, 'website', this.webId, 'page', this.pageId, 'widget', wgid]);
+  }
 }
