2e7e261bd82634bd863a91617a27a9e89f65f693
jennyjohns <johns.j@husky.neu.edu>
2017-10-24 16:35:39 -0400

    userservice code clean up

--- src/app/services/user.service.client.ts
+++ src/app/services/user.service.client.ts
@@ -1,21 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Response } from '@angular/http';
 import 'rxjs/Rx';
-import { environment } from '../../environments/environment';
-import { Router } from '@angular/router';
-import {map} from "rxjs/operator/map";
+
 
 @Injectable()
 
 export class UserService {
   constructor(private http: Http) {}
-  users = [
-    {_id: '123', username: 'alice', password: 'alice', firstName: 'Alice', lastName: 'Wonder', email: 'alice@email.com'},
-    {_id: '234', username: 'bob', password: 'bob', firstName: 'Bob', lastName: 'Marley', email: 'bob@email.com'},
-    {_id: '345', username: 'charly',   password: 'charly',   firstName: 'Charly', lastName: 'Garcia', email: 'charly@email.com'  },
-    {_id: '456', username: 'jannunzi', password: 'jannunzi', firstName: 'Jose',   lastName: 'Annunzi', email: 'jose@email.com' }
-
-  ];
   api = {
     'createUser'   : this.createUser,
     'findUserById' : this.findUserById,
@@ -63,11 +54,6 @@ export class UserService {
       .map((response: Response) => {
         return response.json();
       });
-    // for (let x = 0; x < this.users.length; x++) {
-    //   if (this.users[x]._id === userId) {
-    //     this.users[x] = user;
-    //   }
-    // }
   }
 
   deleteUser(userId: string) {
