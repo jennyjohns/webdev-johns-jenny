dcbc3a6da7f2d120a4ecbcca2cbbdc0e11e2158a
jennyjohns <johns.j@husky.neu.edu>
2017-11-05 19:41:23 -0500

    trying to debug image upload

--- server/services/widget.service.server.js
+++ server/services/widget.service.server.js
@@ -14,23 +14,6 @@ module.exports = function (app) {
   app.delete("/api/widget/:wgid", deleteWidget);
   app.post("/api/upload", upload.single('myFile'), uploadImage);
 
-
-  widgets = [
-    {_id: '123', widgetType: 'HEADING', pageId: '321', size: 2, text: 'GIZMODO'},
-    {_id: '234', widgetType: 'HEADING', pageId: '321', size: 4, text: 'Lorem ipsum'},
-    {
-      _id: '345', widgetType: 'IMAGE', pageId: '321', width: '100%',
-      url: 'http://lorempixel.com/1200/600/'
-    },
-    {_id: '456', widgetType: 'HTML', pageId: '321', text: '<p>Lorem ipsum</p>'},
-    {_id: '567', widgetType: 'HEADING', pageId: '321', size: 4, text: 'Lorem ipsum'},
-    {
-      _id: '678', widgetType: 'YOUTUBE', pageId: '321', width: '100%',
-      url: 'https://youtu.be/AM2Ivdi9c4E'
-    },
-    {_id: '789', widgetType: 'HTML', pageId: '321', text: '<p>Lorem ipsum</p>'}
-  ];
-
   function findAllWidgets(req, res) {
     res.json(widgets);
   }
@@ -102,6 +85,7 @@ module.exports = function (app) {
 
   function uploadImage(req, res) {
     var widgetId = req.body.widgetId;
+    console.log(widgetId);
     var width = req.body.width;
     var myFile = req.file;
     var userId = req.body.userId;
@@ -116,16 +100,18 @@ module.exports = function (app) {
     var mimetype = myFile.mimetype;
 
     widget = getWidgetById(widgetId);
-    widget.url = '/assets/uploads/' + filename;
+    console.log(widget);
+    //widget.url = '/assets/uploads/' + filename;
 
      var callbackUrl = '/user/' + userId + '/website/' + websiteId + '/page/' + pageId + '/widget';
      res.redirect(callbackUrl);
   }
 
   function getWidgetById(widgetId) {
-    var widget = widgets.find(function (widget) {
-      return widget._id === widgetId;
-    });
-    return widget;
+    widgetModel
+      .findWidgetById(widgetId)
+      .then(function (widget) {
+        res.json(widget);
+      });
   }
 };
--- src/app/services/widget.service.client.ts
+++ src/app/services/widget.service.client.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
-import {Http, RequestOptions, Response} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import 'rxjs/Rx';
 import {environment} from '../../environments/environment';
-import {Router} from '@angular/router';
-import {DomSanitizer} from '@angular/platform-browser';
+
 
 // injecting service into module
 @Injectable()
